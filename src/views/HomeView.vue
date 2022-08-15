f8b3aec916ca45082bc680a557a27aa390bb2b26
sreyneven <90229523+sreyneven@users.noreply.github.com>
2022-08-15 08:02:54 +0700

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Hello this home page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import '../index.css'
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
